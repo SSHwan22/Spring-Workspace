a254a085ebf0c198409ee93e2ebbded72e8f3f59
SSU <hshwan0406@gmail.com>
2023-05-01 15:18:33 +0900

    05-01

--- SpringProject/src/main/java/com/kh/spring/board/model/vo/Board.java
+++ SpringProject/src/main/java/com/kh/spring/board/model/vo/Board.java
@@ -9,7 +9,7 @@ public class Board {
 	
 	private int boardNo;
 	private String boardTitle;
-	private String gboardWriter;
+	private String boardWriter;
 	private String boardContent;
 	private int count;
 	private String createDate; // 원하는 형태의 날짜로 얻어오기 위해서 문자열로 바꿈(2023년 4월 28일)
@@ -26,8 +26,5 @@ public class Board {
 	private String profileImage;
 	
 	private ArrayList<BoardImg> imgList;
-	
-	
-	
 	
 }
